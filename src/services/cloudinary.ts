97a8af8a378364aeb851c9cf5cce60ae77dfe401
Mayra Caroline <mayracarol727@gmail.com>
2026-04-07 00:07:18 -0300

    pdf configuração

--- src/services/cloudinary.ts
+++ src/services/cloudinary.ts
@@ -4,29 +4,36 @@ export async function uploadFile(file: File) {
   formData.append("file", file);
   formData.append("upload_preset", "avivai_upload");
 
-  // 👇 ISSO LIBERA O ARQUIVO
-  formData.append("access_mode", "public");
-
   let uploadUrl = "";
 
   if (file.type.includes("video")) {
     uploadUrl = "https://api.cloudinary.com/v1_1/djawb7xgu/video/upload";
   } 
   else if (file.type.includes("pdf")) {
-    uploadUrl = "https://api.cloudinary.com/v1_1/djawb7xgu/raw/upload";
+    uploadUrl = "https://api.cloudinary.com/v1_1/djawb7xgu/image/upload";
   } 
   else {
     uploadUrl = "https://api.cloudinary.com/v1_1/djawb7xgu/image/upload";
   }
 
-  const res = await fetch(uploadUrl, {
-    method: "POST",
-    body: formData,
-  });
+  try {
+    const res = await fetch(uploadUrl, {
+      method: "POST",
+      body: formData,
+    });
 
-  const data = await res.json();
+    const data = await res.json();
 
-  console.log("UPLOAD RESULT:", data);
+    console.log("UPLOAD RESULT:", data);
 
-  return data.secure_url;
+    if (!data.secure_url) {
+      throw new Error("Upload falhou");
+    }
+
+    return data.secure_url;
+
+  } catch (error) {
+    console.error("Erro upload:", error);
+    return null;
+  }
 }
