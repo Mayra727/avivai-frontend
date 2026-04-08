47c4b6b1f550a9a2d56a9ebd21fc0173011024aa
Mayra Caroline <mayracarol727@gmail.com>
2026-04-07 23:02:00 -0300

    pdf

--- src/services/uploadPdf.ts
+++ src/services/uploadPdf.ts
@@ -3,18 +3,19 @@ import { supabase } from "./supabase";
 export async function uploadPdf(file: File) {
   const fileName = `${Date.now()}-${file.name}`;
 
-  const { error } = await supabase.storage
+  const { data, error } = await supabase.storage
     .from("PDF")
     .upload(fileName, file);
 
   if (error) {
-    console.error("Erro upload PDF:", error);
+    console.error("ERRO REAL PDF:", error);
+    alert("Erro real: " + error.message); // 👈 MOSTRA O ERRO
     return null;
   }
 
-  const { data } = supabase.storage
+  const { data: publicUrl } = supabase.storage
     .from("PDF")
     .getPublicUrl(fileName);
 
-  return data.publicUrl;
+  return publicUrl.publicUrl;
 }
