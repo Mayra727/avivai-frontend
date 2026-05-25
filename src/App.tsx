8b9a730934791455d875d0248bb132092c3862f9
Mayra Caroline <mayracarol727@gmail.com>
2026-05-25 20:36:22 -0300

    fix default route redirecting users to producer dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ export default function App() {
   element={<ResetPassword />}
 />
 
-          <Route path="" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/meus-cursos" element={<MyCourses />} />
 
           <Route path="/cursos" element={<Courses />} />
